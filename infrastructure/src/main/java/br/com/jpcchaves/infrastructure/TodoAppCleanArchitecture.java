5484741b55276e92a69d1b98d66af13d3be98111
jpcchaves <joao.chaves@liferay.com>
2024-04-15 13:44:47 -0300

    feat: add main entrypoint for spring boot app

---
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/TodoAppCleanArchitecture.java
@@ -0,0 +1,11 @@
+package br.com.jpcchaves.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppCleanArchitecture {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppCleanArchitecture.class, args);
+    }
+}
