90d57f99cfc73531c56166f4358a0a34b13d592a
jpcchaves <joao.chaves@liferay.com>
2024-04-17 12:05:18 -0300

    feat: add status code attribute in TodoException

--- core/src/main/java/br/com/jpcchaves/core/exception/TodoException.java
+++ core/src/main/java/br/com/jpcchaves/core/exception/TodoException.java
@@ -2,9 +2,20 @@ package br.com.jpcchaves.core.exception;
 
 public class TodoException extends RuntimeException {
   private String code;
+  private int statusCode;
 
   public TodoException(String message, String code) {
     super(message);
     this.code = code;
+  }
+
+  public TodoException(String message, String code, int statusCode) {
+    super(message);
+    this.code = code;
+    this.statusCode = statusCode;
+  }
+
+  public int getStatusCode() {
+    return statusCode;
   }
 }
