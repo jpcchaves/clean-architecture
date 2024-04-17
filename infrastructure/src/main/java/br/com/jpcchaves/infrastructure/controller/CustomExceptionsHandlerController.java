4a2b40b9414b8c668c612bd6c0c5c825b9674dee
jpcchaves <joao.chaves@liferay.com>
2024-04-17 16:23:03 -0300

    feat: add method argument not valid exception handling

--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/controller/CustomExceptionsHandlerController.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/controller/CustomExceptionsHandlerController.java
@@ -3,15 +3,21 @@ package br.com.jpcchaves.infrastructure.controller;
 import br.com.jpcchaves.core.exception.TodoException;
 import br.com.jpcchaves.infrastructure.dto.ExceptionResponseDTO;
 import java.util.Date;
+import java.util.Objects;
 import java.util.logging.Logger;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class CustomExceptionsHandlerController {
+public class CustomExceptionsHandlerController extends ResponseEntityExceptionHandler {
   private static final Logger _logger =
       Logger.getLogger(CustomExceptionsHandlerController.class.getName());
 
@@ -35,5 +41,22 @@ public class CustomExceptionsHandlerController {
         new ExceptionResponseDTO(new Date(), ex.getMessage(), request.getDescription(false));
 
     return new ResponseEntity<>(exceptionResponse, HttpStatus.valueOf(ex.getStatusCode()));
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      @NonNull HttpHeaders headers,
+      @NonNull HttpStatusCode status,
+      WebRequest request) {
+    _logger.severe("Error: " + ex.getClass() + " Message: " + ex.getMessage());
+
+    ExceptionResponseDTO exceptionResponse =
+        new ExceptionResponseDTO(
+            new Date(),
+            Objects.requireNonNull(ex.getFieldError()).getDefaultMessage(),
+            request.getDescription(false));
+
+    return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
   }
 }
