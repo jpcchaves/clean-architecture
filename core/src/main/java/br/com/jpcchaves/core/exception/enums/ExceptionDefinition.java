2ed90be01bdcfeb049f264f7a8286ee8db6a3594
jpcchaves <joao.chaves@liferay.com>
2024-04-19 09:43:29 -0300

    feat: add http status enum

--- core/src/main/java/br/com/jpcchaves/core/exception/enums/ExceptionDefinition.java
+++ core/src/main/java/br/com/jpcchaves/core/exception/enums/ExceptionDefinition.java
@@ -1,7 +1,9 @@
 package br.com.jpcchaves.core.exception.enums;
 
+import br.com.jpcchaves.core.domain.enums.HttpStatus;
+
 public enum ExceptionDefinition {
-  TD0001("TODO not found with the given ID", "TD-0001", 404);
+  TD0001("TODO not found with the given ID", "TD-0001", HttpStatus.NOT_FOUND.value());
 
   private String message;
   private String code;
