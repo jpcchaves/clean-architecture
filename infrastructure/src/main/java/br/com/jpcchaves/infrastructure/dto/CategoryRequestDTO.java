46a3493ba096d0ce1feec2ce5d9c8cfd00064b8c
jpcchaves <joao.chaves@liferay.com>
2024-04-22 09:42:30 -0300

    feat: add dtos for category

---
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/dto/CategoryRequestDTO.java
@@ -0,0 +1,17 @@
+package br.com.jpcchaves.infrastructure.dto;
+
+public class CategoryRequestDTO {
+  private String name;
+
+  public CategoryRequestDTO(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/dto/CategoryResponseDTO.java
@@ -0,0 +1,29 @@
+package br.com.jpcchaves.infrastructure.dto;
+
+public class CategoryResponseDTO {
+  private Long id;
+  private String name;
+
+  public CategoryResponseDTO() {}
+
+  public CategoryResponseDTO(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
