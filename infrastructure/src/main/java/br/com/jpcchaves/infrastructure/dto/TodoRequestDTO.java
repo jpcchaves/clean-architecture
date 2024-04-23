1cc6f340cba6e6896e0301582f7c62b32899509c
jpcchaves <joao.chaves@liferay.com>
2024-04-23 16:41:47 -0300

    fix: adjust validation for category id

--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/dto/TodoRequestDTO.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/dto/TodoRequestDTO.java
@@ -1,12 +1,15 @@
 package br.com.jpcchaves.infrastructure.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public class TodoRequestDTO {
   @NotBlank(message = "The to do is a required field!")
   private String todo = null;
 
-  @NotBlank(message = "The category is a required field!")
+  @NotNull(message = "The category cannot be null!")
+  @Min(value = 0, message = "The category is a required field!")
   private Long categoryId;
 
   public TodoRequestDTO() {}
