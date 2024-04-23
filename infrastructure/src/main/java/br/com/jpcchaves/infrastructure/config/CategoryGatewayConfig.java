64515eb55a95baf00ab7bbb00a279c456eab66cf
jpcchaves <joao.chaves@liferay.com>
2024-04-23 09:03:22 -0300

    feat: add get category by id gateway impl and config

--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/config/CategoryGatewayConfig.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/config/CategoryGatewayConfig.java
@@ -1,10 +1,13 @@
 package br.com.jpcchaves.infrastructure.config;
 
 import br.com.jpcchaves.application.usecaseimpl.category.CreateCategoryUseCaseImpl;
+import br.com.jpcchaves.application.usecaseimpl.category.GetCategoryByIdUseCaseImpl;
 import br.com.jpcchaves.application.usecaseimpl.category.ListCategoriesUseCaseImpl;
 import br.com.jpcchaves.infrastructure.gatewayimpl.category.CreateCategoryGatewayImpl;
+import br.com.jpcchaves.infrastructure.gatewayimpl.category.GetCategoryByIdGatewayImpl;
 import br.com.jpcchaves.infrastructure.gatewayimpl.category.ListCategoriesGatewayImpl;
 import br.com.jpcchaves.usecase.category.CreateCategoryUseCase;
+import br.com.jpcchaves.usecase.category.GetCategoryByIdUseCase;
 import br.com.jpcchaves.usecase.category.ListCategoriesUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,5 +25,11 @@ public class CategoryGatewayConfig {
   public ListCategoriesUseCase listCategoriesUseCase(
       ListCategoriesGatewayImpl listCategoriesGatewayImpl) {
     return new ListCategoriesUseCaseImpl(listCategoriesGatewayImpl);
+  }
+
+  @Bean
+  public GetCategoryByIdUseCase getCategoryByIdUseCase(
+      GetCategoryByIdGatewayImpl getCategoryByIdGatewayImpl) {
+    return new GetCategoryByIdUseCaseImpl(getCategoryByIdGatewayImpl);
   }
 }
