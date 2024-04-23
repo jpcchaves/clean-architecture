8f92671ea319c659c57be7a3af2795545cd7c210
jpcchaves <joao.chaves@liferay.com>
2024-04-23 09:03:52 -0300

    feat: add endpoint to get category by id

--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/controller/CategoryController.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/controller/CategoryController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +31,10 @@ public class CategoryController {
   @GetMapping
   public ResponseEntity<List<CategoryResponseDTO>> list() {
     return ResponseEntity.ok(categoryService.list());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<CategoryResponseDTO> getById(@PathVariable(name = "id") Long id) {
+    return ResponseEntity.ok(categoryService.getById(id));
   }
 }
