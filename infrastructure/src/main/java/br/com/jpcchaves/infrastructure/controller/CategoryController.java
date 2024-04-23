c7d78d13c72ff0f65d19894897e59070ac003f5a
jpcchaves <joao.chaves@liferay.com>
2024-04-23 08:48:39 -0300

    feat: add category list endpoint

--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/controller/CategoryController.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/controller/CategoryController.java
@@ -3,8 +3,10 @@ package br.com.jpcchaves.infrastructure.controller;
 import br.com.jpcchaves.infrastructure.dto.CategoryRequestDTO;
 import br.com.jpcchaves.infrastructure.dto.CategoryResponseDTO;
 import br.com.jpcchaves.infrastructure.service.category.CategoryService;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,10 @@ public class CategoryController {
   @PostMapping
   public ResponseEntity<CategoryResponseDTO> create(@RequestBody CategoryRequestDTO requestDTO) {
     return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.create(requestDTO));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<CategoryResponseDTO>> list() {
+    return ResponseEntity.ok(categoryService.list());
   }
 }
