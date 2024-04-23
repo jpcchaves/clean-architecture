24698dea47a6c0c11fdfca28c37a601f79b1d6e7
jpcchaves <joao.chaves@liferay.com>
2024-04-23 08:48:30 -0300

    feat: impl service for list categories

--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/service/category/CategoryService.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/service/category/CategoryService.java
@@ -2,7 +2,10 @@ package br.com.jpcchaves.infrastructure.service.category;
 
 import br.com.jpcchaves.infrastructure.dto.CategoryRequestDTO;
 import br.com.jpcchaves.infrastructure.dto.CategoryResponseDTO;
+import java.util.List;
 
 public interface CategoryService {
   CategoryResponseDTO create(CategoryRequestDTO category);
+
+  List<CategoryResponseDTO> list();
 }
--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/service/category/CategoryServiceImpl.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/service/category/CategoryServiceImpl.java
@@ -5,24 +5,39 @@ import br.com.jpcchaves.infrastructure.dto.CategoryRequestDTO;
 import br.com.jpcchaves.infrastructure.dto.CategoryResponseDTO;
 import br.com.jpcchaves.infrastructure.mapper.CategoryMapper;
 import br.com.jpcchaves.usecase.category.CreateCategoryUseCase;
+import br.com.jpcchaves.usecase.category.ListCategoriesUseCase;
+import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
   private final CreateCategoryUseCase createCategoryUseCase;
+  private final ListCategoriesUseCase listCategoriesUseCase;
   private final CategoryMapper categoryMapper;
 
   public CategoryServiceImpl(
-      CreateCategoryUseCase createCategoryUseCase, CategoryMapper categoryMapper) {
+      CreateCategoryUseCase createCategoryUseCase,
+      ListCategoriesUseCase listCategoriesUseCase,
+      CategoryMapper categoryMapper) {
     this.createCategoryUseCase = createCategoryUseCase;
+    this.listCategoriesUseCase = listCategoriesUseCase;
     this.categoryMapper = categoryMapper;
   }
 
   @Override
+  @Transactional
   public CategoryResponseDTO create(CategoryRequestDTO category) {
     Category createdCategory =
         createCategoryUseCase.create(categoryMapper.toCoreCategory(category));
 
     return categoryMapper.toDto(createdCategory);
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public List<CategoryResponseDTO> list() {
+    List<Category> categoryList = listCategoriesUseCase.list();
+    return categoryMapper.toCategoryResDTOList(categoryList);
   }
 }
