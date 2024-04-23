404358910a02c8cbfea1a0d1d6b131b64cccc450
jpcchaves <joao.chaves@liferay.com>
2024-04-23 16:34:42 -0300

    feat: injected config for todo req dto to core todo mapping

--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/config/MapperConfig.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/config/MapperConfig.java
@@ -1,5 +1,6 @@
 package br.com.jpcchaves.infrastructure.config;
 
+import br.com.jpcchaves.infrastructure.mapper.TodoDTOMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,10 @@ public class MapperConfig {
 
   @Bean
   public ModelMapper modelMapper() {
-    return new ModelMapper();
+    ModelMapper modelMapper = new ModelMapper();
+
+    modelMapper.addMappings(new TodoDTOMapper());
+
+    return modelMapper;
   }
 }
