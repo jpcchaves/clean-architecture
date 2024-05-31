ee728ba9f74c8ee6620be5cb3c122578993f1dae
jpcchaves <joao.chaves@liferay.com>
2024-05-31 14:32:03 -0300

    add category converter
    Important Note: You need to remember that in case your entities have a
    bidirectional relationship, you need to tell mapstruct to ignore it or
    it will enter an infinite loop and throw an weird exception
    StackOverflowError

--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/mapper/converters/CategoryConverter.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/mapper/converters/CategoryConverter.java
@@ -6,13 +6,18 @@ import br.com.jpcchaves.infrastructure.dto.category.CategoryResponseDTO;
 import br.com.jpcchaves.infrastructure.persistence.entity.CategoryEntity;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
 
 @Mapper(componentModel = ComponentModel.SPRING)
 public interface CategoryConverter {
 
+  @Mapping(target = "todoList", ignore = true)
   Category toCoreCategory(CategoryEntity createdCategory);
 
+  @Mapping(target = "todoList", ignore = true)
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "name", source = "name")
   Category toCoreCategory(CategoryRequestDTO categoryRequestDTO);
 
   CategoryEntity toCategoryEntity(Category category);
