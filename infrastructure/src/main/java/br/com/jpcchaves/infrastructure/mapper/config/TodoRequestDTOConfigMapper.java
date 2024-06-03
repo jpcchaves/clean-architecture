1b3651b47cc9f037915afd670fddf691fe750b56
jpcchaves <joao.chaves@liferay.com>
2024-06-03 15:15:39 -0300

    rename to make it explicit that it's related to config

--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/mapper/config/TodoRequestDTOConfigMapper.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/mapper/config/TodoRequestDTOConfigMapper.java
@@ -1,10 +1,11 @@
-package br.com.jpcchaves.infrastructure.mapper;
+package br.com.jpcchaves.infrastructure.mapper.config;
 
 import br.com.jpcchaves.core.domain.Todo;
 import br.com.jpcchaves.infrastructure.dto.todo.TodoRequestDTO;
 import org.modelmapper.PropertyMap;
 
-public class TodoRequestDTOMapper extends PropertyMap<TodoRequestDTO, Todo> {
+public class TodoRequestDTOConfigMapper extends PropertyMap<TodoRequestDTO, Todo> {
+
   protected void configure() {
     skip().setId(null);
   }
--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/mapper/config/TodoResponseDTOConfigMapper.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/mapper/config/TodoResponseDTOConfigMapper.java
@@ -1,10 +1,11 @@
-package br.com.jpcchaves.infrastructure.mapper;
+package br.com.jpcchaves.infrastructure.mapper.config;
 
 import br.com.jpcchaves.core.domain.Todo;
 import br.com.jpcchaves.infrastructure.dto.todo.TodoResponseDTO;
 import org.modelmapper.PropertyMap;
 
-public class TodoResponseDTOMapper extends PropertyMap<Todo, TodoResponseDTO> {
+public class TodoResponseDTOConfigMapper extends PropertyMap<Todo, TodoResponseDTO> {
+
   @Override
   protected void configure() {
     map().setCategory(source.getCategory().getName());
