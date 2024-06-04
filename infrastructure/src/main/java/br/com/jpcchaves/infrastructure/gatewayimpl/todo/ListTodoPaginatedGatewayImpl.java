33c3d332ceae7741292ed53de08b3b58f50eacd9
jpcchaves <joao.chaves@liferay.com>
2024-06-04 09:02:27 -0300

    apply paginated response builder pattern in usages

--- infrastructure/src/main/java/br/com/jpcchaves/infrastructure/gatewayimpl/todo/ListTodoPaginatedGatewayImpl.java
+++ infrastructure/src/main/java/br/com/jpcchaves/infrastructure/gatewayimpl/todo/ListTodoPaginatedGatewayImpl.java
@@ -7,7 +7,7 @@ import br.com.jpcchaves.core.domain.Todo;
 import br.com.jpcchaves.infrastructure.mapper.contracts.ITodoMapper;
 import br.com.jpcchaves.infrastructure.persistence.entity.TodoEntity;
 import br.com.jpcchaves.infrastructure.persistence.repository.ITodoRepository;
-import br.com.jpcchaves.infrastructure.utils.PaginationUtils;
+import br.com.jpcchaves.infrastructure.utils.pagination.PaginationUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,27 +15,33 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ListTodoPaginatedGatewayImpl implements ListTodoPaginatedGateway {
+
   @Qualifier("${application.config.libraries.repo}")
   private final ITodoRepository<TodoEntity, Long> todoRepository;
 
   private final ITodoMapper todoMapper;
 
   public ListTodoPaginatedGatewayImpl(
-      ITodoRepository<TodoEntity, Long> todoRepository, ITodoMapper todoMapper) {
+      ITodoRepository<TodoEntity, Long> todoRepository,
+      ITodoMapper todoMapper) {
     this.todoRepository = todoRepository;
     this.todoMapper = todoMapper;
   }
 
   @Override
   public PaginatedResponse<Todo> list(PaginationRequest paginationRequest) {
+
     Pageable pageable = PaginationUtils.convertToPageable(paginationRequest);
+
     Page<TodoEntity> todoEntityPage = todoRepository.findAll(pageable);
-    return new PaginatedResponse<>(
-        todoMapper.toTodoList(todoEntityPage.getContent()),
-        todoEntityPage.getNumber(),
-        todoEntityPage.getSize(),
-        todoEntityPage.getTotalElements(),
-        todoEntityPage.getTotalPages(),
-        todoEntityPage.isLast());
+
+    return new PaginatedResponse.Builder<Todo>()
+        .setContent(todoMapper.toTodoList(todoEntityPage.getContent()))
+        .setPage(todoEntityPage.getNumber())
+        .setSize(todoEntityPage.getSize())
+        .setTotalElements(todoEntityPage.getTotalElements())
+        .setTotalPages(todoEntityPage.getTotalPages())
+        .isLast(todoEntityPage.isLast())
+        .build();
   }
 }
